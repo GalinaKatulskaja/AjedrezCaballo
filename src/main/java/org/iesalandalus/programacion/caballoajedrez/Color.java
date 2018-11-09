f0a1b09546c8d9691bb43f420cb3a7d94e22d194
Galina K. <galinakatulskaja1228@list.ru>
2018-11-09 17:06:31 +0100

    1) Creamos ENUM Color

---
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Color.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.iesalandalus.programacion.caballoajedrez;
+
+/**
+ *
+ * @author Galina
+ */
+public enum Color {
+    //Crea un enumerado Color, que contenga los valores: BLANCO y NEGRO.
+    BLANCO, NEGRO;
+}
