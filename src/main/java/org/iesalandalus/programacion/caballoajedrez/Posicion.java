fbc3c7c4761ef77782b7739058cf19dc05d98c05
Galina K. <galinakatulskaja1228@list.ru>
2018-11-09 17:17:31 +0100

    1) Creamos  los métodos get y set para los atributos, que lance una excepción del tipo IllegalArgumentException

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
@@ -13,4 +13,44 @@ public class Posicion {
     //Crea la clase Posicion. Crea los atributos fila (int) y columna (char) con la visibilidad adecuada.
     private int fila;
     private char columna;
+
+    /**Crea los métodos get y set para los atributos. Recuerda que para 
+    el método set se debe tener en cuenta que los valores son correctos 
+    (las filas van del 1 al 8 -ambos inclusive- y las columnas 
+    de la 'a' a la 'h' -ambos inclusive-)  y si no se lance 
+    una excepción del tipo IllegalArgumentException con el mensaje
+    adecuado y no modifique los atributos. */
+    public int getFila() {
+        return fila;
+    }
+
+    public void setFila(int fila) {
+        if(fila<1 | fila>8)
+        {
+            throw new IllegalArgumentException ("ERROR. La fila introducida no es correcta");
+        }
+        else
+        {
+        this.fila = fila;
+        }
+    }
+
+    public char getColumna() {
+        return columna;
+    }
+
+    public void setColumna(char columna) {
+        if(columna!='a'& columna!='b' & columna!='c'& columna!='d' & columna!='e' & columna!='f' & columna!='g' & columna!='h')
+        {
+            throw new IllegalArgumentException ("ERROR. La columna introducida no es correcta");
+        }
+        else
+        {
+        this.columna = columna;
+        }
+    }
+    
+    
+    
+    
 }
